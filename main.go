f3854df2093b9064d7fcb301e8cf83526efca751
timichal <michal@zlatkovsky.cz>
2018-07-13 09:45:29 +0200

    initial drawing test

--- main.go
+++ main.go
@@ -4,10 +4,39 @@ Main file of the Morana game
 package main
 
 import (
-	"fmt"
-	"math"
+	"github.com/nsf/termbox-go"
 )
 
 func main() {
-	fmt.Printf("This is a test %g\n", math.Sqrt(1))
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	defer termbox.Close()
+
+	for i := 0; i<= 100; i++ {
+		termbox.SetCell(i, 0, 'x', termbox.ColorGreen, termbox.ColorBlack)
+		termbox.SetCell(i, 30, 'x', termbox.ColorGreen, termbox.ColorBlack)
+		for j := 1; j < 30; j++ {
+			termbox.SetCell(0, j, 'x', termbox.ColorGreen, termbox.ColorBlack)
+			termbox.SetCell(100, j, 'x', termbox.ColorGreen, termbox.ColorBlack)
+		}
+	}
+	termbox.Flush()
+	MainLoop()
+}
+
+func MainLoop() {
+	for {
+		switch event := termbox.PollEvent(); event.Type {
+		case termbox.EventKey:
+			if event.Key == termbox.KeyEsc {
+				return
+			}
+			if event.Ch == 'q' {
+				return
+			}
+		}
+	}
 }
